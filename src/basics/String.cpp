060c6d463a634c73d66bb8e7dd664c1d254f22bc
sagn-ng <sangnn2510@gmail.com>
2026-08-22 23:00:03 +0700

    updated a method in strings

--- src/basics/String.cpp
+++ src/basics/String.cpp
@@ -9,6 +9,8 @@ int main(){
     string fullname=lastname+' '+firstname;
     cout<<fullname<<endl;
     
+    cout<<"First letter of the string \"firstname\": "<<firstname[0]<<'\n'; //or the at() method
+
     //a string in C++ is actually an object, and we can also use the append() method for concatenation:
     string fullname2=lastname.append(firstname); //lastname is now also concatenated
     cout<<fullname2<<endl;
